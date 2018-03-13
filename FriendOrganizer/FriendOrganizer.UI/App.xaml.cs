98007fabedc5056cead2201f667a72cff4bb42ae
Sotnyk <serge.sotnyk@gmail.com>
2018-03-13 18:14:49 +0200

    After 09.02. Process Unhandled Exceptions

--- FriendOrganizer/FriendOrganizer.UI/App.xaml.cs
+++ FriendOrganizer/FriendOrganizer.UI/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Autofac;
 using FriendOrganizer.UI.Startup;
+using System;
 using System.Windows;
 
 namespace FriendOrganizer.UI
@@ -14,6 +15,14 @@ namespace FriendOrganizer.UI
             var container = new Bootstrapper().Bootstrap();
             var mainWindow = container.Resolve<MainWindow>();
             mainWindow.Show();
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, 
+            System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("Unexpected error occured. Please inform the admin." +
+                Environment.NewLine + e.Exception.Message, "Unexpecten error");
+            e.Handled = true;
         }
     }
 }
